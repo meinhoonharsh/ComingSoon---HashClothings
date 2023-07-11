6112b54026503c9ff07ca8d59d21fa889d140870
meinhoonharsh <harshprogrammer782@gmail.com>
2023-07-11 21:19:23 +0530

    Updated Date

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@
 
   const cfg = {
     // Countdown Timer Final Date
-    finalDate: "July 10, 2023 00:00:00",
+    finalDate: "July 16, 2023 12:00:00",
     // MailChimp URL
     mailChimpURL:
       "https://facebook.us1.list-manage.com/subscribe/post?u=1abf75f6981256963a47d197a&amp;id=37c6d8f4d6",
